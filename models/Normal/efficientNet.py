48f8545783966c383b6c3b600fbe37a15ea8ae3c
ShowLo <945519267@qq.com>
2021-06-04 10:37:35 +0800

    update efficientNet.py

--- models/Normal/efficientNet.py
+++ models/Normal/efficientNet.py
@@ -9,19 +9,7 @@ from torch import nn
 import torch
 import math
 
-# from ..ensure_divisible import ensure_divisible
-def ensure_divisible(number, divisor=8, min_value=None):
-    '''
-    Ensure that 'number' can be 'divisor' divisible
-    Reference from original tensorflow repo:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    '''
-    if min_value is None:
-        min_value = divisor
-    new_num = max(min_value, int(number + divisor / 2) // divisor * divisor)
-    if new_num < 0.9 * number:
-        new_num += divisor
-    return new_num
+from ..ensure_divisible import ensure_divisible
 
 
 class Swish(nn.Module):
